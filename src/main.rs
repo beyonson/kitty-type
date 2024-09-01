af77b3f732df04b94426c0a495d9f7ae533374ba
Garrett <garrettmarcinak@gmail.com>
2024-08-31 20:40:32 -0400

    Get initial set length mode working

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,29 @@ use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
+
 fn main() {
     // Declare stdio
-    let stdin = stdin();
     let mut stdout = stdout().into_raw_mode().unwrap();
 
+    let stdin = stdin();
+    let mut start_row = 3;
     let mut cursor_x = 1;
-    let mut cursor_y = 1;
+    let mut cursor_y = start_row;
     let mut buffer: String = "".to_owned();
+    let test_text = "kitty type";
+
+    write!(
+        stdout,
+        "{}{}{}{}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+        test_text,
+        termion::cursor::Hide
+        //termion::clear::CurrentLine
+    )
+    .unwrap();
+    stdout.flush().unwrap();
 
     for k in stdin.keys() {
         write!(
@@ -38,10 +53,22 @@ fn main() {
             _ => {}
         }
 
+        if buffer == test_text {
+            write!(
+                stdout,
+                "{}{}",
+                "You done",
+                termion::cursor::Goto(1, cursor_y+1)
+            )
+            .unwrap();
+            stdout.flush().unwrap();
+            break;
+        }
+
         // Increment cursor but stop on second line
-        if cursor_x > 80 && cursor_y < 2 {
+        if cursor_x > 80 && cursor_y < start_row + 1 {
             cursor_x = 1;
-            cursor_y = 2;
+            cursor_y += 1;
         } else if cursor_x > 80 {
             cursor_x = 1;
         } else {
